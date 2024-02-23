b92cf8a0165e9ad0d4a7f13a486341fd33db6c64
Junaid Razzaq <142839488+Stayrelax84@users.noreply.github.com>
2024-02-23 19:56:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var a = +prompt("Enter Your Percentage")
+
+var percentage = a
+
+if (percentage > 79.99) {
+    alert("Excellent You Got A+")
+}
+else if (percentage > 69.99){
+    alert("Congratulation! You Got A")
+}
+else if (percentage > 59.99) {
+    alert("Congratulation! You Got B")
+}
+else if (percentage > 49.99){
+    alert("Congratulation! You Got C")
+}
+else{alert("Sorry You Fail")}
